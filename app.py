6c42b168528f7ae3268b4ca25170c584f8ccdffe
Franciska Englert <englert.franciska@gmail.com>
2023-11-29 09:59:55 +0000

    Added streamlit canvas for drawing digits

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from fastbook import *
+from streamlit_drawable_canvas import st_canvas
+
 
 st.markdown("""# This is a header""")
-line_count = st.slider("Select a line count", 1, 10, 3)
 
 path = Path()
 learner = load_learner(path/"digit_classifier_tiny.pkl")
@@ -13,3 +14,31 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     prediction = learner.predict(tensor(image))
     st.markdown(prediction)
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=3,
+    stroke_color='#eee',
+    background_color='#000000',
+    update_streamlit=True,
+    height=200,
+    width=200,
+    drawing_mode="freedraw",
+    display_toolbar=True,
+    key="full_app",
+)
+
+# Do something interesting with the image data and paths
+if canvas_result.image_data is not None:
+    st.image(canvas_result.image_data)
+    st.markdown(canvas_result.image_data)
+    rgba_image = Image.fromarray(canvas_result.image_data)
+    rgb_image = rgba_image.convert('RGB')
+    prediction = learner.predict(tensor(rgb_image))
+    st.markdown(prediction)
+if canvas_result.json_data is not None:
+    objects = pd.json_normalize(canvas_result.json_data["objects"])
+    for col in objects.select_dtypes(include=["object"]).columns:
+        objects[col] = objects[col].astype("str")
+    st.dataframe(objects)
